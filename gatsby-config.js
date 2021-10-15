17c370b8e0f60306cf0676f3e5c78d2e4b9afbcb
Rhys Malyon <malyonr@gmail.com>
2021-10-15 18:40:22 +0900

    Set up default favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,22 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Socious",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "xxx",
+        short_name: "xxxx",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/App-icon.svg" // This path is relative to the root of the site.
+      }
+    },
+  ],
 };
