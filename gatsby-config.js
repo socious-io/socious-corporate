19b30ea5234040fb283daae7970a630fce66e98b
Rhys Malyon <malyonr@gmail.com>
2021-10-15 20:14:27 +0900

    Adjust gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,34 @@
-import { 
-  title,
-  titleAlt,
-  description,
-  headline,
-  siteLanguage,
-  logo,
-  ogLanguage,
-  url,
-  author
-} from './config/website';
+const website = require('./config/website')
 
-export const siteMetadata = {
-  siteUrl: url + pathPrefix, pathPrefix,
-  title: title,
-  titleAlt: titleAlt,
-  description: description,
-  headline: headline,
-  siteLanguage: siteLanguage,
-  logo: logo,
-  ogLanguage: ogLanguage,
-  author: author
-};
-
-export const plugins = [
-  "gatsby-plugin-sass",
-  "gatsby-plugin-sharp",
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: "xxx",
-      short_name: "xxxx",
-      start_url: "/",
-      background_color: "#6b37bf",
-      theme_color: "#6b37bf",
-      // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-      // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-      display: "standalone",
-      icon: "src/images/App-icon.svg" // This path is relative to the root of the site.
-    }
+module.exports = {
+  siteMetadata: {
+    siteUrl: website.url + pathPrefix, // For gatsby-plugin-sitemap
+    pathPrefix,
+    title: website.title,
+    titleAlt: website.titleAlt,
+    description: website.description,
+    headline: website.headline,
+    siteLanguage: website.siteLanguage,
+    logo: website.logo,
+    ogLanguage: website.ogLanguage,
+    author: website.author
   },
-];
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "xxx",
+        short_name: "xxxx",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/App-icon.svg" // This path is relative to the root of the site.
+      }
+    },
+  ],
+};
