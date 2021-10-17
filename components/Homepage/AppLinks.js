ce80dbaa5d76cdfe25368e942bda3daace9bd35b
Rhys Malyon <malyonr@gmail.com>
2021-10-17 17:54:26 +0900

    Create AppLinks component

---
+++ components/Homepage/AppLinks.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const AppLinks = () => {
+  return (
+    <div class="section-app-links">
+      <div class="section-app-links__wrapper">
+        <div class="section-app-links__header">
+          Experience the social impact ecosystem
+        </div>
+        <div class="section-app-links__subheader">
+          Connect with like-minded people, find the right social enterprise, and contribute to solving social issues
+        </div>
+        <div class="section-app-links__app-links-block">
+          <div class="section-app-links__app-links">
+            <button class="section-app-links__app-links-button">
+              <span class="material-icons md-36">apple</span>
+            </button>
+          </div>
+          <div class="section-app-links__app-links">
+            <button class="section-app-links__app-links-button">
+              <span class="material-icons md-36">android</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AppLinks
