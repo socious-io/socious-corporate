a9557ba845cc7b7b354d699de7ac72d0b8aa8783
Rhys Malyon <malyonr@gmail.com>
2021-10-17 18:55:59 +0900

    Create test data for data mapping

---
+++ data/jobSectionLeft.js
@@ -0,0 +1,30 @@
+const jobItem = [
+  {
+    icon: `Whatshot`,
+    title: "Meet like-minded people",
+    subtitle: `
+      Socious AI will match you with the people you want to meet
+      <br>Community support and meaningful connections accelerate social change
+    `
+  },
+  {
+    icon: `DeveloperMode`,
+    title: "Easily find the most suited social impact projects",
+    subtitle: `
+      From fast-growing social startups to international organizations,
+      <br>Socious AI will connect you with the right organization
+      <br>You can contribute to the achievement of the SDGs in a variety of ways,
+      <br>including freelance, part-time, and advisory
+    `
+  },
+  {
+    icon: `FilterTiltShift`,
+    title: "Visualizes your social impact",
+    subtitle: `
+      Socious' unique algorithm quantifies and visualizes
+      <br>your contribution to solving social issues
+    `
+  }
+]
+
+export default jobItem
