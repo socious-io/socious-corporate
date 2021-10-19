26203a77d5ca19e82d99a33a3fd037ef37a4eb8e
Rhys Malyon <malyonr@gmail.com>
2021-10-19 13:32:03 +0900

    Adjust responsive styling

--- components/Homepage/JobsSection/JobsSection.js
+++ components/Homepage/JobsSection/JobsSection.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Job from "./Job";
 import jobsSectionLeft from "../../../data/jobSectionLeft"
+import jobsSectionRight from "../../../data/jobSectionLeft"
 
 const JobsSection = () => {
   const jobItemsLeft = jobsSectionLeft.map(job => 
@@ -11,7 +12,16 @@ const JobsSection = () => {
         title={job.title}
         subtitle={job.subtitle}
       />
-    )
+  )
+
+  const jobItemsRight = jobsSectionRight.map(job => 
+      <Job
+        icon={job.icon}
+        key={job.id}
+        title={job.title}
+        subtitle={job.subtitle}
+      />
+  )
   
   return (
     <div class="section-jobs">
@@ -26,6 +36,25 @@ const JobsSection = () => {
         <div class="section-jobs__img-container">
           <div class="section-jobs__img"></div>
           <div class="section-jobs__img-paleblue-square"></div>
+        </div>
+      </div>
+      <div class="section-jobs__block">
+        <div class="section-jobs__img-entrepreneurs-container">
+          <div class="section-jobs__img-entrepreneurs"></div>
+          <div class="section-jobs__img-entrepreneurs-paleblue-square"></div>
+          <div class="section-jobs__img-entrepreneurs-purple-square"></div>
+        </div>
+        <div class="section-jobs__list">
+          <div class="section-jobs__sup-header">BENEFITS</div>
+          <div class="section-jobs__header">
+            Social entrepreneurs can:
+            {jobItemsRight}
+          </div>
+        </div>
+        <div class="section-jobs__img-entrepreneurs-container--mobile">
+          <div class="section-jobs__img-entrepreneurs--mobile"></div>
+          <div class="section-jobs__img-entrepreneurs-paleblue-square"></div>
+          <div class="section-jobs__img-entrepreneurs-purple-square"></div>
         </div>
       </div>
     </div>
