db74a9d701f39fc8592113e71b26237eafa0df7a
Rhys Malyon <malyonr@gmail.com>
2021-10-19 19:27:51 +0900

    Add conditional classes based on location

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,13 +1,21 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { useLocation } from '@reach/router';
 
 const Navbar = () => {
+  const location = useLocation();
+  console.log(location)
+
+  const headerStyle = location.pathname === '/' ? 'header' : 'header-fixed'
+  const logoImageStyle = location.pathname === '/' ? 'header__logo-image' : 'header__logo-image-transparent'
+  const languageSwitcherStyle = location.pathname === '/' ? 'header__language-switcher' : 'header__language-switcher-transparent'
+  
   return (
-    <nav className="header">
+    <nav className={headerStyle}>
       <div className="header__logo-container">
-        <img className="header__logo-image" src="images/socious-logo.png" />
+        <img className={logoImageStyle} src="images/socious-logo.png" />
       </div>
-      <a className="header__language-switcher" href="./ja">日本語</a>
+      <a className={languageSwitcherStyle} href="./ja">日本語</a>
     </nav>
   )
 }
