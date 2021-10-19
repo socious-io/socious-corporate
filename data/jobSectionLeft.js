cc262a49827e464242b46e8443d94f1e2eadc9e6
Rhys Malyon <malyonr@gmail.com>
2021-10-19 12:38:58 +0900

    Create data file for job components

--- data/jobSectionLeft.js
+++ data/jobSectionLeft.js
@@ -1,28 +1,32 @@
+import React from "react"
+
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import FilterTiltShiftIcon from '@mui/icons-material/FilterTiltShift';
+
 const jobItem = [
   {
-    icon: `Whatshot`,
+    id: 1,
+    icon: <WhatshotIcon />,
     title: "Meet like-minded people",
     subtitle: `
-      Socious AI will match you with the people you want to meet
-      <br>Community support and meaningful connections accelerate social change
+      Socious AI will match you with the people you want to meet and offer community support and meaningful connections that accelerate social change
     `
   },
   {
-    icon: `DeveloperMode`,
+    id: 2,
+    icon: <DeveloperModeIcon />,
     title: "Easily find the most suited social impact projects",
     subtitle: `
-      From fast-growing social startups to international organizations,
-      <br>Socious AI will connect you with the right organization
-      <br>You can contribute to the achievement of the SDGs in a variety of ways,
-      <br>including freelance, part-time, and advisory
+      From fast-growing social startups to international organizations, Socious AI will connect you with the right organization. You can contribute to the achievement of the SDGs in a variety of ways, including freelance, part-time, and advisory.
     `
   },
   {
-    icon: `FilterTiltShift`,
+    id: 3,
+    icon: <FilterTiltShiftIcon />,
     title: "Visualizes your social impact",
     subtitle: `
-      Socious' unique algorithm quantifies and visualizes
-      <br>your contribution to solving social issues
+      Socious' unique algorithm quantifies and visualizes your contribution to solving social issues
     `
   }
 ]
