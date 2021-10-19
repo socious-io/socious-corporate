e88655bfff179fceabced6799f4338d7b70dbc93
Rhys Malyon <malyonr@gmail.com>
2021-10-19 15:33:31 +0900

    Render content in layout

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 
-const Layout = () => {
+import Navbar from './shared/Navbar'
+import Footer from './shared/Footer'
 
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <div>
+      <title>{pageTitle} | { data.site.siteMetadata.title }</title>
+      <Navbar />
+        { children }
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
