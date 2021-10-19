ebd8703ff42767cb571acfea145f180dc5d757a4
Rhys Malyon <malyonr@gmail.com>
2021-10-19 21:50:03 +0900

    Add layout to about component

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+    </Layout>
+  )
+}
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,8 @@ import "../styles/main.scss"
 import Layout from "../components/Layout"
 
 import Intro from "../components/Homepage/Intro/Intro"
-import Navbar from "../components/shared/Navbar"
 import AppLinks from "../components/Homepage/AppLinks"
 import JobsSection from "../components/Homepage/JobsSection/JobsSection"
-import Footer from "../components/shared/Footer"
-
 
 const IndexPage = () => {
   return (
