2c2abb4dce38a0032c2b24ceecf2e83441202800
Rhys Malyon <malyonr@gmail.com>
2021-10-21 20:24:38 +0900

    Add scroll arrow

---
+++ src/components/shared/ScrollArrow.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+
+const ScrollArrow = () => {
+  useEffect(() => {
+    const scrollArrow = document.getElementById("scroll-arrow");
+
+    const myScrollFunc = () => {
+      let y = window.scrollY;
+      if (y >= 800) {
+        scrollArrow.className = "scroll-arrow up show"
+      } else {
+        scrollArrow.className = "scroll-arrow up hide"
+      }
+    };
+
+    window.addEventListener("scroll", myScrollFunc);
+  })
+
+
+  return (
+    <div>
+      <a href="#scroll-top">
+        <i id="scroll-arrow" class="scroll-arrow up hide"></i>
+      </a>
+    </div>
+  )
+}
+
+export default ScrollArrow
