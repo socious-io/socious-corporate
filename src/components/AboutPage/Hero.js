d48bc0a1cc23251ce863af71b67f5750ba3c6d36
Rhys Malyon <malyonr@gmail.com>
2021-10-21 19:57:48 +0900

    Add hero banner

---
+++ src/components/AboutPage/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div class="hero-section" id="our-mission">
+      <div class="hero-text">
+        <h5>OUR MISSION</h5>
+        <h1>Accelerate social change by bringing people together</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Hero from "../components/AboutPage/Hero";
+
 import Layout from "../components/Layout";
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About">
+      <Hero />
     </Layout>
   )
 }
