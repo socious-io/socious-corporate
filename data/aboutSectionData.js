e958dec2a4b1c8029998d06918a7561016cc1d01
Rhys Malyon <malyonr@gmail.com>
2021-10-21 23:42:31 +0900

    Create about item data file

---
+++ data/aboutSectionData.js
@@ -0,0 +1,25 @@
+const aboutItems = [
+  {
+    id: 1,
+    imageUrl: '/images/collaborate.png',
+    imageAlt: 'illustration of people working together',
+    header: 'The resources are scattered',
+    content: 'People who are passionate about the same social cause are not connected and unable to collaborate with each other.'
+  },
+  {
+    id: 2,
+    imageUrl: '/images/environment.png',
+    imageAlt: 'illustration of environmental considerations',
+    header: 'The resources are unmatched',
+    content: 'People passionate about social change are not working in the right roles where they can maximize their full potential.'
+  },
+  {
+    id: 3,
+    imageUrl: '/images/development.png',
+    imageAlt: 'illustration of teamwork and development',
+    header: 'The resources are limited',
+    content: 'While many are interested in contributing to social impact projects, only few are able to do so.'
+  },
+]
+
+export default aboutItems
