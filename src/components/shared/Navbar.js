b311d4fe3a58df8df87ae5c32b51b328ed0f9cba
Rhys Malyon <malyonr@gmail.com>
2021-10-22 14:18:51 +0900

    Update nav links

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -7,14 +7,42 @@ const Navbar = () => {
 
   const headerStyle = location.pathname === '/' ? 'header' : 'header header-fixed'
   const logoImageStyle = location.pathname === '/' ? 'header__logo-image' : 'header__logo-image-transparent'
+  const navLinkStyle = location.pathname === '/' ? 'nav-item' : 'nav-item-transparent'
   const languageSwitcherStyle = location.pathname === '/' ? 'header__language-switcher' : 'header__language-switcher-transparent'
+
+  const activeLinkStyle = {
+    paddingBottom: "0.5rem",
+    borderBottom: "2px solid white"
+  }
   
   return (
     <nav className={headerStyle}>
       <div className="header__logo-container">
-        <img className={logoImageStyle} src="images/socious-logo.png" />
+        <Link to="/">
+          <img className={logoImageStyle} src="images/socious-logo.png" />
+        </Link>
       </div>
-      <a className={languageSwitcherStyle} href="./ja">日本語</a>
+      <div className="nav-links">
+        <div className={navLinkStyle}>
+          <Link 
+          to="/about"
+          activeStyle={activeLinkStyle}
+          partiallyActive={true}
+          >
+            About
+          </Link>
+        </div>
+        <div className={navLinkStyle}>
+          <Link 
+          to="/careers"
+          activeStyle={activeLinkStyle}
+          partiallyActive={true}
+          >
+            Careers
+          </Link>
+        </div>
+        <a className={languageSwitcherStyle} href="./about-ja">日本語</a>
+      </div>
     </nav>
   )
 }
