c49966ec20ad6f8b81e7fc9d8bc21d32e9e9e3dc
Rhys Malyon <malyonr@gmail.com>
2021-10-22 16:33:30 +0900

    Create basic component

---
+++ src/pages/careers/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+
+const CareerPage = () => {
+  return (
+    <Layout pageTitle="Careers">
+
+    </Layout>
+  )
+}
+
+export default CareerPage
