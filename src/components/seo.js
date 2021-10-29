c74b5b3cbb50ac4b8a0273a5635b37f597ba4164
Rhys Malyon <malyonr@gmail.com>
2021-10-30 01:26:29 +0900

    Create SEO component

---
+++ src/components/seo.js
@@ -0,0 +1,96 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
+import { useLocation } from "@reach/router"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Seo = ({ title, description, image, article }) => {
+  const { pathname } = useLocation()
+  const { site } = useStaticQuery(query)
+
+  const {
+    defaultTitle,
+    titleTemplate,
+    defaultDescription,
+    siteUrl,
+    defaultImage,
+    twitterImage,
+    favicon,
+  } = site.siteMetadata
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image || defaultImage}`,
+    url: `${siteUrl}${pathname}`,
+    twitterImage: `${siteUrl}${twitterImage || defaultImage}`,
+  }
+
+  return (
+    <Helmet title={seo.title} titleTemplate={titleTemplate}>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <meta name='og:type' content='website' />
+      {seo.url && <meta property="og:url" content={seo.url} />}
+      {(article ? true : null) && <meta property="og:type" content="article" />}
+      {seo.title && <meta property="og:title" content={seo.title} />}
+      {seo.description && (
+        <meta property="og:description" content={seo.description} />
+      )}
+      {seo.image && <meta property="og:image" content={seo.image} />}
+      <meta name="twitter:card" content="summary_large_image" />
+      {seo.title && <meta name="twitter:title" content={seo.title} />}
+      {seo.description && (
+        <meta name="twitter:description" content={seo.description} />
+      )}
+      {seo.image && <meta name="twitter:image" content={seo.twitterImage} />}
+
+      <link rel="apple-touch-icon" sizes="180x180" href={favicon.appleTouchIcon} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon.ico} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon.sm} />
+      <link rel="icon" type="image/png" sizes="32x32" href={favicon.lg} />
+      <link rel="mask-icon" href={favicon.safariPinnedTab} color="#5bbad5" />
+      <link rel="manifest" href={favicon.manifest} />
+      <meta name="msapplication-TileColor" content="#2b5797" />
+      <meta name="theme-color" content="#ffffff" />
+    </Helmet>
+  )
+}
+
+export default Seo
+
+Seo.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  article: PropTypes.bool,
+}
+
+Seo.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+  article: false,
+}
+
+const query = graphql`
+  query Seo {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        titleTemplate
+        defaultDescription: description
+        siteUrl: url
+        defaultImage: image
+        twitterImage
+        favicon {
+          ico
+          sm
+          lg
+          appleTouchIcon
+          manifest
+        }
+      }
+    }
+  }
+`
