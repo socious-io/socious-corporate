b61980b3521c0b25c7fdce6fbfa9d960761f043c
Rhys Malyon <malyonr@gmail.com>
2021-10-30 15:00:03 +0900

    Revert "Revert "Banner component and styling""
    This reverts commit 44dc95b8be64d9902d6a476eab20701ddcef6d5f.

---
+++ src/components/NewsroomPage/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div class="newsroom-banner">
+      <div class="newsroom-banner__text">
+        <h1>Newsroom</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/newsroom/index.js
+++ src/pages/newsroom/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import Layout from "../../components/Layout";
 import Seo from "../../components/seo"
 
+import Banner from "../../components/NewsroomPage/Banner";
+
 const NewsroomPage = () => {
   return (
     <Layout>
@@ -10,6 +12,7 @@ const NewsroomPage = () => {
         title='Newsroom'
         description='Keep up to date with the latest news at Socious'
       />
+      <Banner />
     </Layout>
   )
 }
