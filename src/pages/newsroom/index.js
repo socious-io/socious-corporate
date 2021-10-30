92e9e1cc619e65788e431994e8e64ea37f31f1e4
Rhys Malyon <malyonr@gmail.com>
2021-10-30 13:12:16 +0900

    Create component

---
+++ src/pages/newsroom/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import Seo from "../components/seo"
+
+const NewsroomPage = () => {
+  return (
+    <Layout>
+      <Seo
+        title='Newsroom'
+        description='Keep up to date with the latest news at Socious'
+      />
+    </Layout>
+  )
+}
+
+export default NewsroomPage
