9e00292f223752480bf2a5a3f095954aaf6601ba
Rhys Malyon <malyonr@gmail.com>
2021-11-01 01:32:26 +0900

    gatsby-source-wordpress initialize

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,11 @@ module.exports = {
        // url: "${process.env.WORDPRESS_URL}/graphql",
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `http://localhost/graphql`,
+      },
+    },
   ],
 };
