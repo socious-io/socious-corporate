a3fccf186c0b380cb8716f34ddda19a757b72641
Rhys Malyon <malyonr@gmail.com>
2021-11-01 01:29:34 +0900

    Graphql query for all posts

--- src/pages/newsroom/index.js
+++ src/pages/newsroom/index.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React from "react"
+import { graphql } from "gatsby"
 
-import Layout from "../../components/Layout";
+import Layout from "../../components/Layout"
 import Seo from "../../components/seo"
 
 import Banner from "../../components/NewsroomPage/Banner";
 
-const NewsroomPage = () => {
+const NewsroomPage = ({ data }) => {
+  console.log(data.wpcontent.posts.nodes)
+  const newsPosts = data.wpcontent.posts.nodes.map(node => {
+    return (
+      <div>
+        <p>{node.title}</p>
+        <small>{node.author.node.name}</small>
+        <div dangerouslySetInnerHTML={{ __html: node.excerpt }}/>
+      </div>
+    )
+  })
+
   return (
     <Layout>
       <Seo
@@ -13,8 +25,29 @@ const NewsroomPage = () => {
         description='Keep up to date with the latest news at Socious'
       />
       <Banner />
+      {newsPosts}
     </Layout>
   )
 }
 
 export default NewsroomPage
+
+export const pageQuery = graphql`
+  query {
+    wpcontent {
+      posts(where: {orderby: {field: DATE, order: DESC}}) {
+        nodes {
+          id
+          uri
+          title
+          author {
+            node {
+              name
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
