606ed8f1720a0d54b26cd6949a89a3605d069894
Rhys Malyon <malyonr@gmail.com>
2021-11-01 23:24:39 +0900

    Comment out blog post component

--- src/pages/newsroom/blog-post.js
+++ src/pages/newsroom/blog-post.js
@@ -1,20 +1,20 @@
-import React from "react"
-import { graphql } from "gatsby"
+// import React from "react"
+// import { graphql } from "gatsby"
 
-import Layout from "../../components/Layout"
+// import Layout from "../../components/Layout"
 
-export default function BlogPost({ data }) {
-  const post = data.allWpPost.nodes[0]
-  console.log(post)
-  return (
-    <Layout>
-      <div>
-        <h1>{post.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />
-      </div>
-    </Layout>
-  )
-}
+// export default function BlogPost({ data }) {
+//   const post = data.allWpPost.nodes[0]
+//   console.log(post)
+//   return (
+//     <Layout>
+//       <div>
+//         <h1>{post.title}</h1>
+//         <div dangerouslySetInnerHTML={{ __html: post.content }} />
+//       </div>
+//     </Layout>
+//   )
+// }
 
 // export const query = graphql`
 //   query($slug: String!) {
