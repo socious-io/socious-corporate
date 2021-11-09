7d638cc0ec98198bd88b2c142313b6229650bfc2
Rhys Malyon <malyonr@gmail.com>
2021-11-09 18:51:32 +0900

    Move some inline styles to stylesheet

--- src/pages/newsroom/PostItem.js
+++ src/pages/newsroom/PostItem.js
@@ -2,20 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 const PostItem = (props) => {
-  const { title, date, author, slug, imageSrc } = props
+  const { title, date, slug, imageSrc } = props
   const url = "/" + slug
 
-  console.log(imageSrc)
-
   return (
-    <div className="article-item">        
-        <Link to={url}>
-          <img src={imageSrc} style = {{ maxWidth: "100%" }} alt="article hero banner" />
-          <h1 style={{ color: "black" }}>{title}</h1>
-          <small style={{color: "grey"}}>{date}</small>
-          <p style={{ color: "black"}}>{author}</p>
-        </Link>
-    </div>
+    <Link to={url}>
+      <div className="news-headline">        
+        <img src={imageSrc} style = {{ maxWidth: "100%" }} alt="article hero banner" />
+        <div className="headline-content">
+          <h3>{title}</h3>
+          <small>{date}</small>
+        </div>
+      </div>
+    </Link>
   )
 }
 
