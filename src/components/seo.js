ccd6abc27193f2cdff311eeafecc6c1175b16430
Rhys Malyon <malyonr@gmail.com>
2021-11-12 16:56:10 +0900

    Fix meta type

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,12 +32,12 @@ const Seo = ({ title, description, image, article }) => {
       <meta name="image" content={seo.image} />
 
       {seo.url && <meta property="og:url" content={seo.url} />}
-      {(article ? true : null) && <meta property="og:type" content="article" />}
       {seo.title && <meta property="og:title" content={seo.title} />}
       {seo.description && (
         <meta property="og:description" content={seo.description} />
-      )}
+        )}
       {seo.image && <meta property="og:image" content={seo.image} />}
+      <meta property="og:type" content="article" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       
