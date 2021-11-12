a74add9fbd5e4f18a2c0d99b6eeda2a40b3d9f9c
Rhys Malyon <malyonr@gmail.com>
2021-11-12 15:39:01 +0900

    Update graphql queries

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -4,9 +4,9 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Seo from '../components/seo'
 
-
 const BlogPost = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark
+  console.log(frontmatter)
 
   return (
     <Layout>
@@ -14,11 +14,16 @@ const BlogPost = ({ data }) => {
         title={frontmatter.title}
         description='Keep up to date with the latest news at Socious'
       />
-      <div id = "main">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.Author.name}</h2>
-        <small>{Date.start}</small>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+      <div className="main">
+        <div className="container__article">
+          <div className="article__info">
+            <h1>{frontmatter.title}</h1>
+            <p>{frontmatter.Date.start}</p>
+            <p>{frontmatter.Author[0].name}</p>
+          </div>
+          <img src={frontmatter.Hero_Image[0].external.url} className="image__article" alt="article hero banner" />
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
       </div>
     </Layout>
   )
@@ -38,6 +43,11 @@ export const query = graphql`
         }
         slug
         title
+        Hero_Image {
+          external {
+            url
+          }
+        }
       }
       html
     }
