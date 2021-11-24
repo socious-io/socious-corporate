915447109da12c3277bd47cf43da8a6e95ca3ec3
Rhys Malyon <malyonr@gmail.com>
2021-11-24 12:37:48 +0900

    Vision section translations

--- src/components/AboutPage/VisionSection.js
+++ src/components/AboutPage/VisionSection.js
@@ -1,20 +1,33 @@
 import React from "react";
 
-const VisionSection = () => {
-  return (
-    <div className="vision-section" id="our-vision">
-      <div className="vision-image">
-        <div className="content-header">
-          <h3>OUR VISION</h3>
-        </div>
+import SimpleLocalize from "../shared/SimpleLocalize";
+import { FormattedMessage } from "react-intl";
 
-      </div>
-      <div className="vision-content">
-        <div className="content-main">
-          <p>Become the world's leading social impact ecosystem</p>
+const VisionSection = (props) => {
+  return (
+    <SimpleLocalize {...props}>
+      <div className="vision-section" id="our-vision">
+        <div className="vision-image">
+          <div className="content-header">
+            <h3>
+              <FormattedMessage
+                id="vision-title"
+              />
+            </h3>
+          </div>
+
+        </div>
+        <div className="vision-content">
+          <div className="content-main">
+            <p>
+              <FormattedMessage
+                id="vision-description"
+              />
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </SimpleLocalize>
   )
 }
 
