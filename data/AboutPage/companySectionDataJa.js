43056be975bb6bc6d40748e77bb389eb3fb76ad6
Rhys Malyon <malyonr@gmail.com>
2021-11-24 14:50:43 +0900

    Add Japanese company info

---
+++ data/AboutPage/companySectionDataJa.js
@@ -0,0 +1,29 @@
+const companyInfoJa = [
+  {
+    id: 1,
+    title: '会社名',
+    info: 'ソーシャス株式会社'
+  },
+  {
+    id: 2,
+    title: '設立',
+    info: '2021年'
+  },
+  {
+    id: 3,
+    title: '代表取締役CEO',
+    info: '尹 世羅'
+  },
+  {
+    id: 4,
+    title: '本社所在地',
+    info: '〒103-0027 東京都中央区日本橋3丁目2番14号1階'
+  },
+  {
+    id: 5,
+    title: '資本金（2021年9月末時点）',
+    info: '3,000,000円'
+  },
+]
+
+export default companyInfoJa
