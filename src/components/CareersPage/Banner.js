de8981016ca808058e9ead1b15e5143e33db85a2
Rhys Malyon <malyonr@gmail.com>
2021-11-24 15:56:22 +0900

    Add career banner Japanese translation

--- src/components/CareersPage/Banner.js
+++ src/components/CareersPage/Banner.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
-const Banner = () => {
+import SimpleLocalize from "../shared/SimpleLocalize";
+import { FormattedMessage } from "react-intl";
+
+const Banner = (props) => {
   return (
-    <div className="career-banner">
-      <div className="career-banner__text">
-        <h1>Careers at Socious</h1>
+    <SimpleLocalize {...props}>
+      <div className="career-banner">
+        <div className="career-banner__text">
+          <h1>
+            <FormattedMessage
+              id="careers-banner"
+            />
+          </h1>
+        </div>
       </div>
-    </div>
+    </SimpleLocalize>
   )
 }
 
