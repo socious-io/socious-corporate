088b756823d9d91ca29e416346f538fb3806b384
Rhys Malyon <malyonr@gmail.com>
2021-11-24 13:57:11 +0900

    Add Japanese translations to values section

--- src/components/AboutPage/ValuesSection.js
+++ src/components/AboutPage/ValuesSection.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import SimpleLocalize from "../shared/SimpleLocalize";
+import { FormattedMessage } from "react-intl";
+
 import ValueItem from "./components/ValueItem";
 import valueItems from "../../../data/AboutPage/valuesSectionData";
+import valueItemsJa from "../../../data/AboutPage/valuesSectionDataJa";
 
-const ValuesSection = () => {
-  const values = valueItems.map(value => 
+const ValuesSection = (props) => {
+  const { language } = props.pageContext
+  const valuesComponentLanguageSelector = language === 'ja' ? valueItemsJa : valueItems
+
+  const values = valuesComponentLanguageSelector.map(value => 
     <ValueItem 
       key={value.id}
       header={value.header}
@@ -13,15 +20,25 @@ const ValuesSection = () => {
   )
 
   return (
-    <section>
-      <div className="values-banner" id="our-values">
-        <h2>Our values</h2>
-        <p>At Socious we want to make a difference in the world but we believe that do so needs to start from within</p>
-      </div>
-      <div className="values-section">
-        {values}
-      </div>
-    </section>
+    <SimpleLocalize {...props}>
+      <section>
+        <div className="values-banner" id="our-values">
+          <h2>
+            <FormattedMessage
+              id="values-title"
+            />
+          </h2>
+          <p>
+            <FormattedMessage
+              id="values-description"
+            />
+          </p>
+        </div>
+        <div className="values-section">
+          {values}
+        </div>
+      </section>
+    </SimpleLocalize>
   )
 }
 
