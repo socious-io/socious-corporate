d8340a4fd0fb712a655890c52bad0f4c80abff61
Rhys Malyon <malyonr@gmail.com>
2021-11-29 20:58:44 +0900

    Create privacy policy page component

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,379 @@
+import React from 'react'
+
+const PrivacyPolicyPage = () => {
+  return (
+    <div id="content">
+        <div class="section">
+            <span>
+              Socious Inc. (hereinafter referred to as 'the Company') provides the following privacy policy (hereinafter
+              referred to as the 'Policy') for the application called 'Socious' and the services on the website (hereinafter
+              referred to as the 'Services') operated by the Company under the domain name 'socious.io/'. The following privacy
+              policy (the 'Policy') is provided for the service on the application and website called 'Socious' operated under the
+              name 'socious.io/' (the 'Service') The following privacy policy (the 'Policy') is provided for the application
+              called 'Socious' and the services on the website (the 'Services'). (hereinafter referred to as the 'Company') will
+              handle user information, including personal information of registered users, in accordance with the following
+              privacy policy (hereinafter referred to as the 'Policy').
+            </span>
+        </div>
+        <div class="section">
+          <span class="section-title">Article 1. User information to be collected and collection method</span>
+          <ol>
+            <li>
+              <span>In this policy, 'personal information' is defined in accordance with the Personal Information Protection Law.</span>
+            </li>
+            <li>
+              <span>
+                In this policy, 'user information' means information related to the identification of the registered user,
+                behavioral history on communication services, and other information generated or accumulated by the registered
+                user or the registered user's terminal on the registered user's smartphone, PC, or other terminal, and collected
+                by the Company in accordance with this policy.
+              </span>
+            </li>
+            <li>
+              <span>The terms used in this Policy shall be in accordance with those defined in the Terms of Service.</span>
+            </li>
+            <li>
+              <span>The User Information collected by the Company in the Service is as follows, depending on the method of collection.</span>
+            </li>
+          </ol>
+          <div>
+            <span class="section-title">(1) Information to be provided at the time of registration</span>
+            <ol class="list-style-circle">
+              <li>
+                <span>Name</span>
+              </li>
+              <li>
+                <span>E-mail address</span>
+              </li>
+              <li>
+                <span>Date of birth</span>
+              </li>
+              <li>
+                <span>Credit card information</span>
+              </li>
+              <li>
+                <span>Photo</span>
+              </li>
+              <li>
+                <span>Other information entered by the registered user in the input form provided by the Company</span>
+              </li>
+            </ol>
+          </div>
+          <div>
+            <span class="section-title">(2) Information provided by other services by allowing registered users to link with other services in the use of this service.</span>
+            <ol class="list-style-circle">
+              <li>
+                <span>ID used by the registered user in the relevant external service</span>
+              </li>
+              <li>
+                <span>
+                  Other information that the registered user is allowed to disclose to the linked party according to the privacy settings
+                  of the external service in question
+                </span>
+              </li>
+            </ol>
+          </div>
+          <div>
+            <span class="section-title">(3) Information collected by the Company when a registered user uses the Service.</span><br/>
+            <span>In addition to (1) and (2), the Company may collect information regarding the access status and usage of the Service. This includes the following information.</span>
+            <ol class="list-style-circle">
+              <li>
+                <span>Terminal information</span>
+              </li>
+              <li>
+                <span>Log information</span>
+              </li>
+              <li>
+                <span>Cookies and anonymous IDs</span>
+              </li>
+              <li>
+                <span>Location information</span>
+              </li>
+            </ol>
+        </div>
+        </div>
+        <div class="section">
+          <span class="section-title">Article 2. Purpose of Use</span><br/>
+          <ol>
+              <li>
+                  <span>The specific purposes of use of user information related to the provision of this service are as follows.</span><br/>
+                  <span>(1) To provide information about the Service and to respond to inquiries.</span><br/>
+                  <span>(2) To respond to violations of the Company's rules, policies, etc. (hereinafter referred to as
+                      'Rules, etc.') related to the Service.</span><br/>
+                  <span>(3) To notify you of changes to the Terms of Service, etc.</span><br/>
+                  <span>(4) To use for purposes incidental to the above.</span>
+              </li>
+              <li>
+                  <span>In addition to the provisions of the preceding paragraph, user information may also be used for the following purposes</span>
+              </li>
+          </ol>
+          <table border="1" cellspacing="0">
+            <tbody><tr>
+              <td>
+                <span>Purpose of use</span>
+              </td>
+              <td>
+                <span>Corresponding user information items</span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span>(1) To create statistical data processed into a format that does not identify individuals in relation to our services.</span>
+              </td>
+              <td>
+                <ol class="list-style-circle">
+                  <li>
+                    <span>Terminal information</span>
+                  </li>
+                  <li>
+                    <span>Log information</span>
+                  </li>
+                </ol>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span>(2) To distribute or display advertisements of the Company or third parties</span>
+              </td>
+              <td>
+                <ol class="list-style-circle">
+                  <li>
+                    <span>Terminal information</span>
+                  </li>
+                  <li>
+                    <span>Log information</span>
+                  </li>
+                </ol>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span>(3) To be used for other marketing purposes</span>
+              </td>
+              <td>
+                <ol class="list-style-circle">
+                  <li>
+                    <span>Name</span>
+                  </li>
+                  <li>
+                    <span>Email address</span>
+                  </li>
+                  <li>
+                    <span>Date of birth</span>
+                  </li>
+                  <li>
+                    <span>Other information entered by the registered user in the input form provided by the Company</span>
+                  </li>
+                  <li>
+                    <span>Submission data</span>
+                  </li>
+                  <li>
+                    <span>Social actions</span>
+                  </li>
+                </ol>
+              </td>
+            </tr>
+          </tbody></table>
+        </div>
+        <div class="section">
+          <span class="section-title">Article 3. Provision to Third Parties</span>
+          <br/>
+          <span>
+            The Company shall not provide personal information among user information to any third party without
+            the prior consent of the registered user, except in cases where disclosure is permitted under the Personal
+            Information Protection Law or other laws and regulations. However, this does not apply to the following cases.
+          </span>
+          <br/>
+          <span>
+            (1) When the Company entrusts all or part of the handling of personal information within the scope
+            necessary to achieve the purpose of use.
+          </span>
+          <br/>
+          <span>
+            (2) When personal information is provided in connection with the succession of a business due to a
+            merger or other reasons
+          </span>
+          <br/>
+          <span>
+            (3) When personal information is provided to an information collection module provider in accordance
+            with the provisions of Section 4.
+          </span>
+          <br/>
+          <span>
+            (4) When it is necessary to cooperate with a national agency, a local government, or a person or
+            organization entrusted by either of the foregoing in executing affairs prescribed by law, and obtaining
+            the consent of the registered user may impede the execution of such affairs
+          </span>
+          <br/>
+          <span>
+            (5) In other cases permitted by the Personal Information Protection Law or other laws and regulations.
+          </span>
+        </div>
+        <div class="section">
+          <span class="section-title">Article 4. External Transmission, Provision to Third Parties, and Information Collection Modules</span><br/>
+          <span>
+            In order to deliver targeted advertisements using user information, the following information collection modules are incorporated
+            in the Service. In accordance with this, the user information will be provided to the information collection module provider as follows.
+          </span>
+          <table border="1" cellspacing="0">
+            <tbody><tr>
+              <td>
+                <span>(1) Name of the information collection module</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <span>(2) Providers of information collection modules</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <span>(3) Items of user information to be provided</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <span>(4) Means and methods of provision</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <span>(5) Purpose of use by the above providers</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <span>(6) Whether or not the above-mentioned provider provides the information to a third party</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <span>(7) URL of the privacy policy of the above provider</span>
+              </td>
+              <td></td>
+            </tr>
+          </tbody></table>
+        </div>
+        <div class="section">
+          <span class="section-title">Article 5. Joint Use</span><br/>
+          <span>The Company will jointly use the personal information of registered users as follows.</span>
+          <table border="1" cellspacing="0">
+            <tbody>
+              <tr>
+                <td>(1) Items of personal information to be used jointly</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>(2) Items of personal information to be used jointly</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>(3) Purposes of use by parties jointly using the information</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>(4) Name or title of the person responsible for the management of user information</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="section">
+          <span class="section-title">Article 6. Disclosure of Personal Information</span><br/>
+          <span>
+            When the Company is requested by a registered user to disclose his/her personal information in accordance with
+            the provisions of the Act on the Protection of Personal Information, the Company shall disclose such information
+            to the registered user without delay after confirming that the request is made by the registered user himself/herself
+            (if the said personal information does not exist, the Company shall notify the registered user to that effect).
+            (If the personal information in question does not exist, the Company will notify the registered user to that effect.
+            (If such personal information does not exist, the Company shall notify the registered user to that effect.)
+            However, this shall not apply in cases where the Company is not obligated to disclose such information under the
+            Personal Information Protection Law or other laws and regulations.
+          </span>
+        </div>
+        <div class="section">
+          <span class="section-title">Article 7. Method of notification, publication, or obtaining consent, and method of requesting discontinuation of use</span>
+          <ol>
+            <li>
+              <span>The Company shall obtain the consent of the registered user before collecting the following user information.</span><br/>
+              <span>(1) Device information</span><br/>
+              <span>(2) Location informatio</span>
+            </li>
+            <li>
+              <span>
+                Registered users may request that the use of all or part of the user information be stopped by setting
+                the prescribed settings for the Service, and in such case, the Company shall promptly stop the use of
+                such information in accordance with the Company's regulations. Please note that for some items of User
+                Information, the collection or use of such information is a prerequisite for the Service, and the
+                cessation of the collection or use of such information will result in the cessation of use of the
+                Service.
+              </span>
+            </li>
+          </ol>
+        </div>
+        <div class="section">
+            <span class="section-title">Article 8. Correction and Suspension of Use of Personal Information</span>
+            <ol>
+              <li>
+                <span>
+                  In the event that a registered user requests us to (1) correct the content of his/her personal information
+                  in accordance with the provisions of the Personal Information Protection Law on the grounds that the information
+                  is not true, or (2) suspend the use of his/her personal information in accordance with the provisions of the
+                  Personal Information Protection Law on the grounds that the information is being handled beyond the scope of the
+                  purpose of use announced in advance, or that the information was collected through deception or other wrongful means,
+                  we will conduct the necessary investigation without delay after confirming that the request is made by the registered user.
+                  If a request is made to stop the use of personal information in accordance with the provisions of the Personal Information
+                  Protection Law on the grounds that (1) the information has been handled in a manner that exceeds the scope of the purpose
+                  of use announced in advance, or (2) the information has been collected through deception or other wrongful means, we will
+                  conduct the necessary investigation without delay after confirming that the request is made by the registered user, and based
+                  on the results of the investigation, we will correct the content of the personal information or stop its use, and notify the
+                  registered user to that effect. If the Company decides not to correct or suspend the use of the personal information based on
+                  reasonable grounds, the Company will notify the registered user to that effect.
+                </span>
+              </li>
+              <li>
+                <span>
+                  If a registered user requests the deletion of the registered user's personal information, and the Company deems it necessary
+                  to comply with the request, the Company will delete the personal information after confirming that the request is made by the
+                  registered user himself/herself, and notify the registered user to that effect.
+                </span>
+              </li>
+              <li>
+                <span>
+                  The provisions of the preceding two paragraphs shall not apply in cases where the Company is not obligated to correct, etc. or
+                  suspend use, etc. under the Personal Information Protection Law or other laws and regulations.
+                </span>
+              </li>
+            </ol>
+        </div>
+        <div class="section">
+            <span class="section-title">Article 9. Contact for Inquiries</span><br/>
+            <span>If you have any questions regarding the handling of user information, please contact the following office.</span><br/>
+            <span>Address: 1F, 3-2-14 Nihonbashi, Chuo-ku, Tokyo 103-0027, Japan<br/>
+                1F, 3-2-14 Nihonbashi, Chuo-ku, Tokyo, Japan</span><br/>
+            <span>Company name: Socious Inc.</span><br/>
+            <span>Department: Customer Support</span><br/>
+            <span>Email: <a href="mailto:support@socious.io">support@socious.io</a></span>
+        </div>
+        <div class="section">
+            <span class="section-title">Article 10. Procedures for Changing the Privacy Policy</span><br/>
+            <span>The Company will review the operation of the handling of user information from time to time, strive for continuous improvement, and may change this policy as necessary. In the event of any changes, the Company shall notify registered users of the effective date and content of the revised policy by posting a notice on the Company's website or by other appropriate means. However, in the case of a change to the content that requires the consent of the registered user under the law, the consent of the registered user shall be obtained in a manner prescribed by the Company.</span>
+        </div>
+        <div class="section">
+            <span class="section-title">Article 11. Language</span><br/>
+            <span>The original version of this Agreement is written in Japanese.</span>
+        </div>
+        <div class="section">
+            <span>Enacted on August 19, 2021</span>
+        </div>
+    </div>
+  )
+}
+
+export default PrivacyPolicyPage
