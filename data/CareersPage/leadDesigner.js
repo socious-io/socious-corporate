4078f67cba63f83933688aed864979459cda0e57
Seira Yun <discoverdeepseira@gmail.com>
2021-12-05 23:30:23 +0900

    Added Lead UI/UX Designer Position

---
+++ data/CareersPage/leadDesigner.js
@@ -0,0 +1,101 @@
+import React from "react";
+
+const leadDesigner = {
+  title: 'Lead UI/UX Designer',
+  location: 'Tokyo, Japan',
+  department: 'Design',
+  body: [
+    {
+      id: 1,
+      header: 'The Role',
+      content:
+        <p>
+          Do you have a passion for social impact? If yes, Socious Inc. is looking for somebody with your enthusiasm and skills to work as part of the team. We are looking for an experienced UI/UX designer who is curious, driven, and passionate about social impact. If you are looking for a role where you can make a major impact in the world, we want to meet you.
+          <br /><br />
+          Socious is a community app for social change makers with a mission to accelerate social change by bringing people
+          together. Many people are interested in contributing to social impact projects, but few of them can do so. We leverage
+          this untapped potential by helping them contribute to society in an easy, fun way through a purpose-driven community
+          app, AI recommendations, and blockchain-based community currency.
+          <br /><br />
+          We started Socious in July 2021, so as an early employee you’ll be pivotal in shaping the direction of the company. We
+          have enough runway to last us through the end of 2022, and are about to close a seed round to allow us to accelerate
+          even faster.
+          <br /><br />
+          You’ll be able to work fully remotely, though we'll meet up in person several times per year, so we're looking for
+          someone living in Japan. If you aren't here yet, that's fine: you’ll be able to work remotely until we help you secure a
+          working visa.
+          <br /><br />
+          We communicate in English, and so no Japanese ability is required, though there will be opportunities to use Japanese if
+          you’d like to.
+          <br /><br />
+          We’ve built a prototype version of our app using a combination of part-time freelance developers and offshore
+          developers. As we secure funding, we’re now looking to bring on our first full time designer.
+          <br /><br />
+          In this role, you’ll work closely with senior stakeholders to understand business goals and ensure that the design team & tools used are aligned with these.
+          As we grow, you’ll have the opportunity to move into a Chief Design Officer role in the
+          future.
+        </p>
+      ,
+    },
+    {
+      id: 2,
+      header: 'Responsibilities',
+      content:
+        <ul>
+          <li>Lead and mentor a team of designers.</li>
+          <li>Deliver on all aspects of UI/UX design, from strategy to execution.</li>
+          <li>Translate user needs, business goals, and technology requirements to drive design decisions.</li>
+          <li>Partner with and influence executive stakeholders.</li>
+          <li>Build a user-centric vision for features across different devices including iOS, Android, and desktop.</li>
+        </ul>
+      ,
+    },
+    {
+      id: 3,
+      header: 'Skills & Experience',
+      content:
+        <ul>
+          <li>10+ years of industry experience (or equivalent skills) as a UI/UX designer and/or manager in a cross-functional UI/UX team.</li>
+          <li>Experience in architecting system wide design solutions that satisfy both product and engineering requirements.</li>
+          <li>Experience in full product life cycle from design through deployment.</li>
+        </ul>
+      ,
+    },
+    {
+      id: 4,
+      header: 'Language Skills',
+      content:
+        <ul>
+          <li>Business-level English skills are required. Japanese skills are desirable.</li>
+        </ul>
+      ,
+    },
+    {
+      id: 5,
+      header: 'What’s on offer',
+      content:
+        <ul>
+          <li>A supportive, trusting and transparent working environment</li>
+          <li>Work-life balance with flexible hours and leave (unlimited paid leave)</li>
+          <li>A place to look at things differently and challenge and offer solutions</li>
+          <li>Flexible work arrangements (full-time, part-time freelance, side-job, onsite, remote, etc.)</li>
+          <li>A competitive salary package (10-20 million yen per year + stock options)</li>
+          <li>Japan work visa sponsorship</li>
+        </ul>
+      ,
+    },
+    {
+      id: 6,
+      header: 'How to apply',
+      content:
+        <div>
+          <p>To apply, please send your letter of motivation (cover letter), resume, and portfolio to <a href="mailto:jobs@socious.io">jobs@socious.io</a></p>
+          <br />
+          <p><strong>Please Note:</strong> This role is open to remote work outside Japan and also to remote work initially, with the aim of relocating to Japan post-Covid.</p>
+        </div>
+      ,
+    },
+  ]
+}
+
+export default leadDesigner
