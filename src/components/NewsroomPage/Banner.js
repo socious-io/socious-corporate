1940e804ed296c28ba8a9a1eb2ab2da598fffa63
Rhys Malyon <malyonr@gmail.com>
2021-12-05 18:54:34 +0900

    Import and render newsroom background image

--- src/components/NewsroomPage/Banner.js
+++ src/components/NewsroomPage/Banner.js
@@ -1,12 +1,39 @@
 import React from 'react'
 
+import { graphql, useStaticQuery } from 'gatsby'
+import { getImage } from 'gatsby-plugin-image'
+import { BgImage } from 'gbimage-bridge'
+
 const Banner = () => {
+  const { newsroomBackgroundImage } = useStaticQuery(
+    graphql`
+      query {
+        newsroomBackgroundImage: file(relativePath: {eq: "newsroom-banner.png"}) {
+          childImageSharp {
+            gatsbyImageData(
+              width: 2000,
+              quality: 100,
+              webpOptions: {quality: 100}
+              placeholder: BLURRED
+            )
+          }
+        }
+      }
+    `
+  )
+
+  const pluginImage = getImage(newsroomBackgroundImage)
+  const backgroundFluidImageStack = [
+    pluginImage,
+    `linear-gradient(rgba(10,10,10,0.8), rgba(10,10,10,0.2))`,
+  ].reverse();
+
   return (
-    <div className="newsroom-banner">
+    <BgImage image={backgroundFluidImageStack} className="newsroom-banner">
       <div className="newsroom-banner__text">
         <h1>Newsroom</h1>
       </div>
-    </div>
+    </BgImage>
   )
 }
 
