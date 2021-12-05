d7762d4a64c7bc9463d2bb3ef9b210a3f6eb5da9
Rhys Malyon <malyonr@gmail.com>
2021-12-05 18:43:00 +0900

    Import and render career background image

--- src/components/CareersPage/Banner.js
+++ src/components/CareersPage/Banner.js
@@ -1,12 +1,39 @@
 import React from 'react'
 
+import { graphql, useStaticQuery } from 'gatsby'
+import { getImage } from 'gatsby-plugin-image'
+import { BgImage } from 'gbimage-bridge'
+
 import SimpleLocalize from "../shared/SimpleLocalize";
 import { FormattedMessage } from "react-intl";
 
 const Banner = (props) => {
+  const { careersBackgroundImage } = useStaticQuery(
+    graphql`
+      query {
+        careersBackgroundImage: file(relativePath: {eq: "careers-banner.png"}) {
+          childImageSharp {
+            gatsbyImageData(
+              width: 2000,
+              quality: 100,
+              webpOptions: {quality: 100}
+              placeholder: BLURRED
+            )
+          }
+        }
+      }
+    `
+  )
+
+  const pluginImage = getImage(careersBackgroundImage)
+  const backgroundFluidImageStack = [
+    pluginImage,
+    `linear-gradient(rgba(10,10,10,0.8), rgba(10,10,10,0.2))`,
+  ].reverse();
+
   return (
     <SimpleLocalize {...props}>
-      <div className="career-banner">
+      <BgImage image={backgroundFluidImageStack} className="career-banner">
         <div className="career-banner__text">
           <h1>
             <FormattedMessage
@@ -14,7 +41,7 @@ const Banner = (props) => {
             />
           </h1>
         </div>
-      </div>
+      </BgImage>
     </SimpleLocalize>
   )
 }
