4b92c41dc2fea638b374e7090acbabd458092e21
Rhys Malyon <malyonr@gmail.com>
2021-12-06 15:59:18 +0900

    Optimize image with StaticImage

--- src/components/Homepage/Intro/MobileImageBlock.js
+++ src/components/Homepage/Intro/MobileImageBlock.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 const MobileImageBlock = () => {
   return (
     <div className="section-intro__block">
-      <div className="section-intro__app-image"></div>
+      <StaticImage
+        src="../../../images/socious-company.png"
+        alt="socious app illustration"
+        className="section-intro__app-image"
+        placeholder="blurred"
+        loading="eager"
+        objectFit="contain"
+      />
       <div className="section-intro__app-image-blue-square"></div>
       <div className="section-intro__app-image-paleblue-square"></div>
     </div>
