0cd4a4e2e5ac29652a8a9cd43666e3cddfb78be7
Seira Yun <discoverdeepseira@gmail.com>
2021-12-30 01:14:35 +0900

    Added the CTO position

---
+++ data/CareersPage/cto.js
@@ -0,0 +1,114 @@
+import React from "react";
+
+const cto = {
+  title: 'Chief Technology Officer (CTO)',
+  location: 'Tokyo, Japan',
+  department: 'Engineering',
+  body: [
+    {
+      id: 1,
+      header: 'The Role',
+      content:
+        <p>
+          Do you have a passion for social impact? If yes, Socious Inc. is looking for somebody with your enthusiasm and skills to work as part of the team. We are looking for an experienced developer who is curious, driven, and passionate about social impact. If you are looking for a role where you can make a major impact in the world, we want to meet you.
+          <br /><br />
+          Socious is a community app for social change, powered by AI and blockchain. Many people are interested in contributing to social impact projects, but few of them can do so. We leverage
+          this untapped potential by helping them contribute to society in an easy, fun way through a purpose-driven community
+          app, AI recommendations, and blockchain-based community currency. We are developing a decentralized application (Dapp) where users can take control of and monetize their content and data.
+          <br /><br />
+          We started Socious in July 2021, so as an early employee you’ll be pivotal in shaping the direction of the company. We
+          have enough runway to last us through the end of 2022, and are about to close a seed round to allow us to accelerate
+          even faster.
+          <br /><br />
+          You’ll be able to work fully remotely, though we'll meet up in person several times per year, so we're looking for
+          someone living in Japan. If you aren't here yet, that's fine: you’ll be able to work remotely until we help you secure a
+          working visa.
+          <br /><br />
+          We communicate in English, and so no Japanese ability is required, though there will be opportunities to use Japanese if
+          you’d like to.
+          <br /><br />
+          You will be working with a team of talented developers (one full-time employee + several freelancers).
+          <br /><br />
+          In this role, you’ll work closely with senior stakeholders to understand business goals and ensure that the development
+          team & technologies used are aligned with these.
+        </p>
+      ,
+    },
+    {
+      id: 2,
+      header: 'Responsibilities',
+      content:
+        <ul>
+          <li>Develop technical aspects of the company’s strategy to ensure alignment with its business goals</li>
+          <li>Discover and implement new technologies that yield competitive advantage</li>
+          <li>Supervise system infrastructure to ensure functionality and efficiency</li>
+          <li>Designing, building and configuring applications to meet business process and application requirements</li>
+          <li>Research, design, develop, and test blockchain technologies</li>
+          <li>Be involved in the global blockchain community—work on implementing and integrating the latest improvement proposals</li>
+          <li>Monitor KPIs and IT budgets to assess technological performance</li>
+          <li>Use stakeholders’ feedback to inform necessary improvements and adjustments to technology</li>
+          <li>Communicate technology strategy to partners and investors</li>
+        </ul>
+      ,
+    },
+    {
+      id: 3,
+      header: 'Skills & Experience',
+      content:
+        <ul>
+          <li>A strong passion for social impact (previous experience with social impact organizations is an asset).</li>
+          <li>Proven experience as a CTO or similar leadership role</li>
+          <li>Minimum 10 years of experience (or equivalent skills) in software/Dapp development, especially with Solidity, C++, Java, JavaScript, Typescript, React Native, React, PHP, NodeJS, MySQL, GraphQL, Go, AWS</li>
+          <li>Strong knowledge of common algorithms and data structures</li>
+          <li>Knowledge of technological trends to build strategy</li>
+          <li>Understanding of budgets and business-planning</li>
+          <li>Familiarity with basic cryptography</li>
+          <li>Familiarity with P2P networks</li>
+          <li>Strong knowledge of blockchains</li>
+          <li>Strong knowledge of concurrency and writing efficient and safe multithreaded code</li>
+          <li>Excellent communication skills</li>
+          <li>Leadership and organizational abilities</li>
+          <li>Strategic thinking</li>
+          <li>Problem-solving aptitude</li>
+          <li>BSc/BA in Computer Science, Engineering or a related field; MBA or other relevant graduate degree is a plus</li>
+        </ul>
+      ,
+    },
+    {
+      id: 4,
+      header: 'Language Skills',
+      content:
+        <ul>
+          <li>Business-level English skills are required. Japanese skills are not required.</li>
+        </ul>
+      ,
+    },
+    {
+      id: 5,
+      header: 'What’s on offer',
+      content:
+        <ul>
+          <li>Diverse, driven, and high-performing colleagues</li>
+          <li>A supportive, trusting and transparent working environment</li>
+          <li>Work-life balance with flexible hours and leave (unlimited paid leave)</li>
+          <li>Flexible work arrangements (full-time, part-time freelance, side-job, onsite, remote, etc.)</li>
+          <li>A competitive salary package (15-30 million yen per year + stock options)</li>
+          <li>Japan work visa sponsorship</li>
+        </ul>
+      ,
+    },
+    {
+      id: 6,
+      header: 'How to apply',
+      content:
+        <div>
+          <p>To apply, please send your letter of motivation (cover letter) and CV to <a href="mailto:jobs@socious.io">jobs@socious.io</a></p>
+          <br />
+          <p><strong>Please Note:</strong> This role is open to remote work outside Japan and also to remote work initially, with the aim of relocating to Japan post-Covid.</p>
+        </div>
+      ,
+    },
+  ]
+}
+
+export default cto
