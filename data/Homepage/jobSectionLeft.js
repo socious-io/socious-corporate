715a4a03c03cfaa797166024ab46f8962c13fc81
Seira Yun <discoverdeepseira@gmail.com>
2022-03-21 12:50:45 +0900

    edited front page socially conscious individuals in English

--- data/Homepage/jobSectionLeft.js
+++ data/Homepage/jobSectionLeft.js
@@ -8,7 +8,7 @@ const jobItem = [
   {
     id: 1,
     icon: <WhatshotIcon />,
-    title: "Discover and contribute to social impact projects you care about effortlessly",
+    title: "Discover and contribute to impact projects",
     subtitle: `Meet like-minded people and be part of the social impact community.
     Find one-off, part-time, and full-time opportunities at social impact organizations tackling various social issues and Sustainable Development Goals (SDGs).
     Our AI recommendations will save you time and hassle.
@@ -17,17 +17,17 @@ const jobItem = [
   {
     id: 2,
     icon: <DeveloperModeIcon />,
-    title: "Find paid opportunities where you can learn new skills while contributing to a social cause",
-    subtitle: `Find opportunities where you can earn fiat currency/SociousCoin (blockchain-based community currency) while learning new skills.
-    Our proprietary algorithms ensure meaningful connections with people you want to meet and reduce the risk of mismatches.
+    title: "Get rewards for your impact",
+    subtitle: `When you contribute your time and money to impact projects, our AI tracks your good deeds and reflect them in your impact credit score.
+    You receive SociousCoin, a blockchain-based community currency, as rewards for your social and environmental impact.
     `
   },
   {
     id: 3,
     icon: <FilterTiltShiftIcon />,
-    title: "Visualize and monetize your impact",
-    subtitle: `Socious' unique algorithm quantifies and visualizes your contribution to solving social issues.
-    Improve your impact credit scores and earn SociousCoin by contributing to social causes.
+    title: "Thank acts of kindness with SociousCoin",
+    subtitle: `You can thank people who have positively impacted your life (mentors, teachers etc.) by sending them SociousCoin.
+    The impact credit scores of both you and the recipient will improve.
     `
   }
 ]
