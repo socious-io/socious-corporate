a627c4b7e38028694341486ec5be8f6d8b3d7ce0
Seira Yun <discoverdeepseira@gmail.com>
2022-04-26 12:18:01 +0200

    edited the company info

--- data/AboutPage/companySectionData.js
+++ data/AboutPage/companySectionData.js
@@ -2,32 +2,12 @@ const companyInfo = [
   {
     id: 1,
     title: 'Company Name',
-    info: 'Socious Inc.'
-  },
-  {
-    id: 2,
-    title: 'Established',
-    info: 'July 2021'
+    info: 'Socious Global Inc.'
   },
   {
     id: 3,
-    title: 'Representative Director/CEO',
+    title: 'Founder & CEO',
     info: 'Seira Yun'
-  },
-  {
-    id: 4,
-    title: 'Address',
-    info: 'Nihonbashi 3-2-14-1F, Chuo Ward, Tokyo, Japan 103-0027'
-  },
-  {
-    id: 5,
-    title: 'Capital (As of 30 September 2021)',
-    info: '3,000,000 yen'
-  },
-  {
-    id: 6,
-    title: 'Legal Advisor',
-    info: 'Anderson Mori & Tomotsune'
   },
 ]
 
