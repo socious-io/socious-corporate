0f439a81749ecc10c6968fbb96e7b3671252f0ec
Seira Yun <discoverdeepseira@gmail.com>
2022-04-26 12:21:13 +0200

    edited out headquorters info

--- data/AboutPage/contactSectionData.js
+++ data/AboutPage/contactSectionData.js
@@ -9,11 +9,6 @@ const contactItems = [
     content: 'info@socious.io'
   },
   {
-    id: 2,
-    header: 'Headquarters',
-    content: 'Nihonbashi 3-2-14-1F, Chuo Ward, Tokyo, Japan 103-0027'
-  },
-  {
     id: 3,
     header: 'For more information',
     content: <a href="https://us6.list-manage.com/contact-form?u=131eb0c7f6b2c3a36daf52940&form_id=8ac2ed4e02b1104cad3be462bb898bb4"><Envelope size={18} /> Drop us a message</a>
