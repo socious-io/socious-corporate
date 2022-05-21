8e02a073accbd4ed76334dbdc47a12868d1166a9
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2022-05-21 11:13:04 +0800

    move position of buttons and forms in homepage

--- src/components/Homepage/AppLinks.js
+++ src/components/Homepage/AppLinks.js
@@ -1,48 +1,136 @@
-import React from 'react'
+import React from "react";
+import addToMailchimp from "gatsby-plugin-mailchimp";
 
 import SimpleLocalize from "../shared/SimpleLocalize";
 import { FormattedMessage } from "react-intl";
 
-import AppleIcon from '@mui/icons-material/Apple';
-import AndroidIcon from '@mui/icons-material/Android';
+import AppleIcon from "@mui/icons-material/Apple";
+import AndroidIcon from "@mui/icons-material/Android";
 
-const AppLinks = (props) => {
-  return (
-    <SimpleLocalize {...props}>
-      <div className="section-app-links">
-        <div className="section-app-links__wrapper">
-          <div className="section-app-links__container">
-            <div className="section-app-links__header">
-              <FormattedMessage
-                id="app-links-header"
-              />
+export default class AppLinks extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fullName: "",
+      email: "",
+    };
+    this._handleChange = this._handleChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleChange = (e) => {
+    this.setState({
+      [`${e.target.name}`]: e.target.value,
+    });
+  };
+
+  _handleSubmit = (e) => {
+    e.preventDefault();
+
+    addToMailchimp(this.state.email, this.state)
+      .then(({ msg, result }) => {
+        console.log("msg", `${result}: ${msg}`);
+
+        if (result !== "success") {
+          throw msg;
+        }
+        alert(msg);
+      })
+      .catch((err) => {
+        console.log("err", err);
+        alert(err);
+      });
+  };
+
+  render() {
+    return (
+      <SimpleLocalize {...this.props}>
+        <div className="section-app-links">
+          <div className="section-app-links__wrapper">
+            <div className="section-app-links__container">
+              <div className="section-app-links__header">
+                <FormattedMessage id="app-links-header" />
+              </div>
+              <div className="section-app-links__subheader">
+                <FormattedMessage id="sign-up-newsletter" />
+              </div>
             </div>
-            <div className="section-app-links__subheader">
-              <FormattedMessage
-                id="app-links-subheader"
-              />
-            </div>
-          </div>
-          <div className="section-app-links__app-links-block">
-            <div className="section-app-links__app-links">
-              <a href="https://apps.apple.com/jp/app/socious/id1581904474" target="_blank" rel="noreferrer">
-                <button className="section-app-links__app-links-button">
-                  <AppleIcon sx={{ fontSize: 48 }} />
-                </button>
-              </a>
-            </div>
-            <div className="section-app-links__app-links">
-              <a href="https://play.google.com/store/apps/details?id=com.dissocialnetwork" target="_blank" rel="noreferrer">
-                <button className="section-app-links__app-links-button">
-                  <AndroidIcon sx={{ fontSize: 48 }} />
-                </button>
-              </a>
+            <div id="mc_embed_signup">
+              <form
+                onSubmit={this._handleSubmit}
+                id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form"
+                className="validate"
+                target="_blank"
+                noValidate
+              >
+                <div id="mc_embed_signup_scroll">
+                  <h2>
+                    <FormattedMessage id="form-title" />
+                  </h2>
+                  <div className="indicates-required">
+                    <span className="asterisk">*</span>
+                    <FormattedMessage id="form-required" />
+                  </div>
+                  <div className="mc-field-group">
+                    <label htmlFor="mce-name">
+                      <FormattedMessage id="form-full-name" />
+                    </label>
+                    <input
+                      type="text"
+                      value={this.state.fullName}
+                      name="fullName"
+                      onChange={this._handleChange}
+                      id="mce-name"
+                    />
+                  </div>
+                  <div className="mc-field-group">
+                    <label htmlFor="mce-EMAIL">
+                      <FormattedMessage id="form-email" />
+                      <span className="asterisk">*</span>
+                    </label>
+                    <input
+                      type="email"
+                      value={this.state.email}
+                      name="email"
+                      onChange={this._handleChange}
+                      className="required email"
+                      id="mce-email"
+                    />
+                  </div>
+                  <div id="mce-responses" className="clear">
+                    <div
+                      className="response"
+                      id="mce-error-response"
+                      style={{ display: "none" }}
+                    ></div>
+                    <div
+                      className="response"
+                      id="mce-success-response"
+                      style={{ display: "none" }}
+                    ></div>
+                  </div>
+                  <div
+                    style={{ position: "absolute", left: "-5000px" }}
+                    aria-hidden="true"
+                  >
+                    <input
+                      type="text"
+                      name="b_131eb0c7f6b2c3a36daf52940_521f139131"
+                      tabIndex="-1"
+                    />
+                  </div>
+                  <div>
+                    <button id="mc-embedded-subscribe" className="button">
+                      <FormattedMessage id="form-button" />
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-      </div>
-    </SimpleLocalize>
-  )
+      </SimpleLocalize>
+    );
+  }
 }
-
-export default AppLinks
--- src/components/Homepage/Intro/EarlyAccessForm.js
+++ src/components/Homepage/Intro/EarlyAccessForm.js
@@ -1,150 +1,65 @@
-import React from 'react'
-import addToMailchimp from 'gatsby-plugin-mailchimp'
+import React from "react";
+import addToMailchimp from "gatsby-plugin-mailchimp";
 
-import SimpleLocalize from '../../shared/SimpleLocalize'
-import { FormattedMessage } from 'react-intl'
+import SimpleLocalize from "../../shared/SimpleLocalize";
+import { FormattedMessage } from "react-intl";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default class EarlyAccessForm extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      fullName: "",
-      email: "",
-    }
-    this._handleChange = this._handleChange.bind(this)
-    this._handleSubmit = this._handleSubmit.bind(this)
-  }
-
-  _handleChange = e => {
-    this.setState({
-      [`${e.target.name}`]: e.target.value,
-    })
-  }
-
-  _handleSubmit = e => {
-    e.preventDefault()
-
-    console.log('submit', this.state)
-
-    addToMailchimp(this.state.email, this.state)
-      .then(({ msg, result }) => {
-        console.log('msg', `${result}: ${msg}`)
-
-        if (result !== 'success') {
-          throw msg
-        }
-        alert(msg)
-      })
-      .catch(err => {
-        console.log('err', err)
-        alert(err)
-      })
-  }
-
   render() {
     return (
       <SimpleLocalize {...this.props}>
-        <div className="section-intro__block">
-          <div className="section-intro__mailchimp">
-            <FormattedMessage
-              id="form-header-black"
-            />
-            <div className="section-intro__mailchimp--blue">
-              <FormattedMessage
-                id="form-header-blue-one"
-              />
+        <div className="section-intro__block intro-right-section">
+          <div className="">
+            <div className="section-intro__mailchimp">
+              <FormattedMessage id="form-header-black" />
+              <div className="section-intro__mailchimp--blue">
+                <FormattedMessage id="form-header-blue-one" />
+              </div>
             </div>
-          </div>
-          <div className="section-intro__mailchimp__form-container">
-            <div id="mc_embed_signup">
-              <form
-                onSubmit={this._handleSubmit}
-                id="mc-embedded-subscribe-form"
-                name="mc-embedded-subscribe-form"
-                className="validate"
-                target="_blank"
-                noValidate
-              >
-                <div id="mc_embed_signup_scroll">
-                  <h2>
-                    <FormattedMessage
-                      id="form-title"
+            <div className="section-app-links__subheader">
+              <FormattedMessage id="app-links-subheader" />
+            </div>
+
+            <div className="section-app-links__app-links-block">
+              <div className="section-app-links__app-links">
+                <a
+                  href="https://apps.apple.com/jp/app/socious/id1581904474"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="section-app-links__app-links-button">
+                    <StaticImage
+                      src="../../../images/apple.png"
+                      alt="Download on Appstore"
+                      loading="eager"
+                      objectFit="cover"
+                      width={180}
                     />
-                  </h2>
-                  <div className="indicates-required">
-                    <span className="asterisk">*</span>
-                    <FormattedMessage
-                      id="form-required"
+                  </button>
+                </a>
+              </div>
+              <div className="section-app-links__app-links">
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.dissocialnetwork"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="section-app-links__app-links-button">
+                    <StaticImage
+                      src="../../../images/google.png"
+                      alt="Get it on Google Play"
+                      loading="eager"
+                      objectFit="cover"
+                      width={180}
                     />
-                  </div>
-                  <div className="mc-field-group">
-                    <label htmlFor="mce-name">
-                      <FormattedMessage
-                        id="form-full-name"
-                      />
-                    </label>
-                    <input
-                      type="text"
-                      value={this.state.fullName}
-                      name="fullName"
-                      onChange={this._handleChange}
-                      id="mce-name"
-                    />
-                  </div>
-                  <div className="mc-field-group">
-                    <label htmlFor="mce-EMAIL">
-                      <FormattedMessage
-                        id="form-email"
-                      />
-                      <span className="asterisk">*</span>
-                    </label>
-                    <input
-                      type="email"
-                      value={this.state.email}
-                      name="email"
-                      onChange={this._handleChange}
-                      className="required email"
-                      id="mce-email"
-                    />
-                  </div>
-                  <div id="mce-responses" className="clear">
-                    <div
-                      className="response"
-                      id="mce-error-response"
-                      style={{display: 'none'}}
-                    ></div>
-                    <div
-                      className="response"
-                      id="mce-success-response"
-                      style={{display: 'none'}}
-                    ></div>
-                  </div>
-                  <div
-                    style={{position: 'absolute', left: '-5000px'}}
-                    aria-hidden="true"
-                  >
-                    <input
-                      type="text"
-                      name="b_131eb0c7f6b2c3a36daf52940_521f139131"
-                      tabIndex="-1"
-                    />
-                  </div>
-                  <div className="clear">
-                    <button
-                      id="mc-embedded-subscribe"
-                      className="button"
-                    >
-                      <FormattedMessage
-                        id="form-button"
-                      />
-                    </button>
-                  </div>
-                </div>
-              </form>
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </SimpleLocalize>
-    )
+    );
   }
 }
