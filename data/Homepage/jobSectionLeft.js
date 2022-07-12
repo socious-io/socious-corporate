54ac14eea1789d010af2a671443f911502e1d90d
Seira Yun <discoverdeepseira@gmail.com>
2022-07-12 18:03:16 +0900

    Copy edit 20220711

--- data/Homepage/jobSectionLeft.js
+++ data/Homepage/jobSectionLeft.js
@@ -8,26 +8,22 @@ const jobItem = [
   {
     id: 1,
     icon: <WhatshotIcon />,
-    title: "Discover and contribute to impact projects",
-    subtitle: `Meet like-minded people and be part of the social impact community.
-    Find one-off, part-time, and full-time opportunities at social impact organizations tackling various social issues and Sustainable Development Goals (SDGs).
-    Our AI recommendations will save you time and hassle.
+    title: "Discover impact projects that help you grow",
+    subtitle: `Find opportunities at impact organizations where you can learn new skills.
     `
   },
   {
     id: 2,
     icon: <DeveloperModeIcon />,
-    title: "Get rewards for your impact",
-    subtitle: `When you contribute your time and money to impact projects, our AI tracks your good deeds and reflect them in your impact credit score.
-    You receive SociousCoin, a blockchain-based community currency, as rewards for your social and environmental impact.
+    title: "Get rewarded for your impact",
+    subtitle: `Contribute and receive THANK tokens as rewards based on your Impact Score.
     `
   },
   {
     id: 3,
     icon: <FilterTiltShiftIcon />,
-    title: "Thank acts of kindness with SociousCoin",
-    subtitle: `You can thank people who have positively impacted your life (mentors, teachers etc.) by sending them SociousCoin.
-    The impact credit scores of both you and the recipient will improve.
+    title: "Be part of the growing community",
+    subtitle: `Meet like-minded people and be part of the impact community.
     `
   }
 ]
