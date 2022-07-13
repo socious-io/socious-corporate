d0d01c755737358d84b74dc5dd6a523e50ca3a5c
Seira Yun <discoverdeepseira@gmail.com>
2022-07-13 18:02:13 +0900

    Copy edit

--- data/AboutPage/aboutSectionData.js
+++ data/AboutPage/aboutSectionData.js
@@ -10,15 +10,15 @@ const aboutItems = [
     id: 2,
     imageUrl: '/images/environment.png',
     imageAlt: 'illustration of environmental considerations',
-    header: 'Impact Credit Sytem and SociousCoin *in development',
-    content: 'Users can contribute their time and money to matched impact projects. Our AI tracks users’ good deeds and calculates their ‘impact credit scores’. Socious rewards their good deeds with SociousCoin, our blockchain-based community currency.'
+    header: 'Impact Gig Marketplace',
+    content: 'We connect impact talents with impact projects that help them grow, visualize their social/environmental impact, and get rewarded for their contributions.'
   },
   {
     id: 3,
     imageUrl: '/images/development.png',
     imageAlt: 'illustration of teamwork and development',
     header: 'Marketplace for socially conscious products *to be developed',
-    content: `In the future, users will also be able to improve their impact credit scores and earn SociousCoin by buying and selling ethical products/carbon credits, creating and consuming content for social impact, and many more.`
+    content: `In the future, users will also be able to improve their impact scores and earn rewards by buying and selling ethical products/carbon credits, creating and consuming content for social impact, and many more.`
   },
 ]
 
