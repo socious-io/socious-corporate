feec6c67a568a430ba78e4dd29d1b60f09e1f849
Dimas Nashiruddin Al Faruq <dimasna96@gmail.com>
2022-08-15 19:05:08 +0700

    new qr code

--- src/components/BlogPage/BlogCard.js
+++ src/components/BlogPage/BlogCard.js
@@ -11,37 +11,37 @@ const BlogCard = ({ edge, alternate }) => {
     featuredImage,
     excerpt,
     date
-  }} = edge
-  const link =edge.node.slug.substring( 0, slug.indexOf( "%" ) )
-  console.log({link});
+  } } = edge
+  const link = edge.node.slug.substring(0, slug.indexOf("%"))
+  console.log({ link });
 
   return (
-    <div  className={ alternate ? "alternate blog" : "blog"}>
-      <img src={ WORDPRESS_URL + featuredImage?.node.sourceUrl} alt={ featuredImage?.node.altText || title} />
+    <div className={alternate ? "alternate blog" : "blog"}>
+      <img src={WORDPRESS_URL + featuredImage?.node.sourceUrl} alt={featuredImage?.node.altText || title} />
       <div className='blog-content-words'>
         <p className='blog--date'>
-          <small>{ date }</small>
+          <small>{date}</small>
         </p>
 
         {alternate ? (
           <h3 className="blog-title">
-            { title }
+            {title}
           </h3>
         ) : (
           <h1 className="blog-title">
-            { title }
+            {title}
           </h1>
         )}
-        { excerpt && 
-        
-          <div className="blog-para" dangerouslySetInnerHTML={{ __html: excerpt }}/>
-        
-          
+        {excerpt &&
+
+          <div className="blog-para" dangerouslySetInnerHTML={{ __html: excerpt }} />
+
+
         }
         <Link to={`/blog/${slug}`}>
           Read more &rarr;
         </Link>
-      </div>            
+      </div>
     </div>
   );
 };
