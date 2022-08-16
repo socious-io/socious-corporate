bb8ffb55742db774c3571bc05efd605a741356a6
Dimas Nashiruddin Al Faruq <dimasna96@gmail.com>
2022-08-16 16:23:57 +0700

    icon and responsive card of reconcile work with purpose

--- src/components/Homepage/JobsSection/Job.js
+++ src/components/Homepage/JobsSection/Job.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Job = (props) => {
   return (
-    <div className="section-jobs__list-item">
-      <div className="section-jobs__list-container">
-      <div className="section-jobs__list-icon">
-        {props.icon}
-      </div>
+    <div className="flex-1 !min-w-[15em] !min-h-[11em] section-jobs__list-item">
+      <div className="!w-[60%] section-jobs__list-container">
+        <div className="flex !h-auto w-auto !p-[10px] section-jobs__list-icon">
+          {props.icon}
+        </div>
         <div className="section-jobs__list-item-title">
           {props.title}
         </div>
