39e2f07e9dacf08073b2a1af56c0de9ecf4eb743
TLab <tlab241@gmail.com>
2022-09-04 08:55:01 +0900

    add: enable to set a aribitrary eventName

--- src/components/segmentUtils.js
+++ src/components/segmentUtils.js
@@ -1,6 +1,9 @@
 // This file defines utils functions to track events with Segment 
 
 // track Button Click Event
-export const trackButtonClick = (event) => {
-  window.analytics.track(`Clicked - ${event.target.innerText}`);
+export const trackButtonClick = (event, eventName=null) => {
+  if (!eventName){
+    eventName = `Clicked - ${event.target.innerText}`;
+  };
+  window.analytics.track(eventName)
 };
